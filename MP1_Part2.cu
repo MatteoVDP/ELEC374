dbd825c945b2e64062cae0f58c2c8a7fcc7110b0
MatteoVDP <107491980+MatteoVDP@users.noreply.github.com>
2024-03-14 02:07:20 -0400

    final working version

--- MP1_Part2.cu
+++ MP1_Part2.cu
@@ -5,11 +5,12 @@
 #include <stdio.h>
 #include <time.h>
 
-#define MATRIX_WIDTH 1000
-#define MATRIX_SIZE (MATRIX_WIDTH * MATRIX_WIDTH)
-#define BLOCK_WIDTH 1 //[2,5,10,25,32]
+#define MATRIX_WIDTH 1000 //dimensions of matrices
+#define MATRIX_SIZE (MATRIX_WIDTH * MATRIX_WIDTH) //total number of elements in matrices
 #define NBYTES (MATRIX_SIZE * sizeof(float))
 
+//int BLOCK_WIDTH = 1;
+int BLOCK_WIDTH[] = { 2, 5, 10, 25, 32 };
 
 float M[MATRIX_SIZE];
 float N[MATRIX_SIZE];
@@ -19,6 +20,7 @@ float P[MATRIX_SIZE];
 void cudaTransferTest();
 void cudaMatMult(float* M, float* N, float* P, int WIDTH);
 
+//matrix multiplication kernel, called by cudaMatMult function
 __global__ void matMultKernel(float* M, float* N, float* P, int WIDTH)
 {
 	// calculate row, col index
@@ -28,7 +30,7 @@ __global__ void matMultKernel(float* M, float* N, float* P, int WIDTH)
 	if (row < WIDTH && col < WIDTH)
 	{
 		float Pvalue = 0;
-
+		//each thread computes one element of the block sub-matrix
 		for (int k = 0; k < WIDTH; k++) {
 			Pvalue += M[row * WIDTH + k] * N[k * WIDTH + col];
 		}
@@ -36,6 +38,31 @@ __global__ void matMultKernel(float* M, float* N, float* P, int WIDTH)
 	}
 }
 
+void checkGPUanswer(float* M, float* N, float* GPU_P, int WIDTH)
+{
+	bool passed;
+	float check;
+
+	for (int i = 0; i < WIDTH; i++)
+	{
+		for (int j = 0; j < WIDTH; j++)
+		{
+			check = 0;
+
+			for (int k = 0; k < WIDTH; k++)
+			{
+				check += M[i * WIDTH + k] * N[k * WIDTH + j];
+				if (GPU_P[i * WIDTH + j] != check) passed = 0;
+			}
+		}
+	}
+	passed = 1;
+
+	if (passed)	printf("TEST PASSED\n");
+	else		printf("TEST FAILED\n");
+}
+
+//standard matrix multiplication, computed using CPU
 void CPUmatMult(float* M, float* N, float* P, int WIDTH)
 {
 	cudaEvent_t start, stop;
@@ -78,8 +105,6 @@ int main()
 	cudaMallocHost((void**)&N, NBYTES);
 	cudaMallocHost((void**)&P, NBYTES);
 
-
-
 	for (int i = 0; i < MATRIX_SIZE; i++)
 	{	// value between 0 and 10, one decimal place
 		M[i] = rand() % 100 / (float) 10.0;
@@ -92,7 +117,7 @@ int main()
 	//for(int i = 0; i < 5; i++)
 	//{
 		cudaMatMult(M, N, P, MATRIX_WIDTH);
-		CPUmatMult(M, N, P, MATRIX_WIDTH);
+	//	CPUmatMult(M, N, P, MATRIX_WIDTH);
 	//}
 
 	cudaFreeHost(M);
@@ -182,25 +207,30 @@ void cudaMatMult(float* M, float* N, float* P, int WIDTH)
 	cudaMemcpy(dM, M, MATRIX_SIZE * sizeof(float), cudaMemcpyHostToDevice);
 	cudaMemcpy(dN, N, MATRIX_SIZE * sizeof(float), cudaMemcpyHostToDevice);
 
-	int NUM_BLOCKS = WIDTH / BLOCK_WIDTH;
-	if (WIDTH % BLOCK_WIDTH) NUM_BLOCKS++;
+	for(int i = 0; i < 5; i++)
+	{
 
-	dim3 dimGrid(NUM_BLOCKS, NUM_BLOCKS);
-	dim3 dimBlock(BLOCK_WIDTH, BLOCK_WIDTH);
+		int NUM_BLOCKS = WIDTH / BLOCK_WIDTH[i];
+		if (WIDTH % BLOCK_WIDTH[i]) NUM_BLOCKS++;
 
-	for (int i = 0; i < 5; i++)
-	{		
-		cudaEventRecord(start, 0); // start timer
-		cudaDeviceSynchronize();
+		dim3 dimGrid(NUM_BLOCKS, NUM_BLOCKS);
+		dim3 dimBlock(BLOCK_WIDTH[i], BLOCK_WIDTH[i]);
 
-		//copy information from device to host
-		matMultKernel <<<dimGrid, dimBlock>>> (dM, dN, dP, WIDTH);
+		for (int i = 0; i < 5; i++)
+		{
+			cudaEventRecord(start, 0); // start timer
+			cudaDeviceSynchronize();
 
-		cudaEventRecord(stop, 0);	// end timer
-		cudaEventSynchronize(stop);
-		cudaEventElapsedTime(&gpu_time, start, stop);
-		printf("Time for GPU matrix multiplication: %f\n", gpu_time); //display results
+			//copy information from device to host
+			matMultKernel <<<dimGrid, dimBlock >>> (dM, dN, dP, WIDTH);
 
+			cudaEventRecord(stop, 0);	// end timer
+			cudaEventSynchronize(stop);
+			cudaEventElapsedTime(&gpu_time, start, stop);
+			printf("Time for GPU matrix multiplication: %f\n", gpu_time); //display results
+			checkGPUanswer(M, N, P, MATRIX_WIDTH);
+
+		}
 	}
 
 	cudaFree(dM);
